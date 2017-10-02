19207f02ba09d59a42e900410fac0ecc77e1fd43
jlamberts <jonathan.lamberts@gmail.com>
2017-10-01 21:58:50 -0400

    functional test now checks that we can input a todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -15,17 +17,33 @@ class NewVisitorTest(unittest.TestCase):
 		#I go to my todo list to add a new item
 		self.browser.get('http://localhost:8000')
 
-		# Glancing at the top of the screen, I notice that the title of the webpage mentions "To-Do"
+		# Glancing at the top of the screen, I notice that the title and header of the webpage mentions "To-Do"
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish this test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-		# I'm immediately asked to enter an iterm
+		# I'm immediately asked to enter an item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item')
 
 		# I type "Work on homework" into the text box
+		inputbox.send_keys('Work on homework')
 
 		# I hit enter, and the page updates.  The list now contains "Work on homework" as its first item
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Work on homework' for row in rows)
+			)
+
+		
 		# There's still a text box, and I enter "Clean Room"
+		self.fail('Finish the test!')
 
 		# The page updates and shows both items
 
